c543e00f1a878aca696c8dd007a992ea38f7d7b5
Parsa <parsa.mostafavi.pm@gmail.com>
2023-11-06 19:12:50 +0330

    function for defining tables

---
+++ database/tables.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const { sequelize, connectDB } = require("../database/connect");
+
+const createTable = () => {
+  connectDB();
+  // difining tables
+};
+
+createTable();  
